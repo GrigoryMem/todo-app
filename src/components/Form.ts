7dff367db39be0a3197554eba9b85368a07e9985
Grigory Morozov <morozehc@yandex.ru>
2025-04-08 15:34:29 +0300

    feat: run form and handle Form

--- src/components/Form.ts
+++ src/components/Form.ts
@@ -7,9 +7,11 @@ export  class Form {
   constructor(formElement: HTMLFormElement, protected handleFormSubmit: Function) {
     this.formElement = formElement;
     this.inputField = this.formElement.querySelector('.todo-form__input') as HTMLInputElement;
-    this.formElement.addEventListener('click',(evt)=>{
+    this.formElement.addEventListener('submit',(evt)=>{
       evt.preventDefault();
+      // обработчик действий пользователя содержится вне класса
       this.handleFormSubmit(this.inputField.value)
+      console.log("this.inputField.value")
     })
   }
   
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,21 @@
 import "./styles/styles.css"
 import {todos} from './utils/constants';
-import {Item} from './components/Item'
+import {Item} from './components/Item';
+import {Form} from './components/Form';
+
 const contentElement = document.querySelector('.todos__list') 
 
 const template = document.querySelector('#todo-item-template') as HTMLTemplateElement;
+const formElement = document.querySelector('.todos__form') as HTMLFormElement;
+
+const todoForm = new Form(formElement,handleSubmitForm);
 
 function handleSubmitForm(data:string){
   // data - строка из поля ввода
     const todoItem = new Item(template);
     const itemElement = todoItem.render(data);
     contentElement.prepend(itemElement);
-    to
+    todoForm.clearValue();
 
 }
 
