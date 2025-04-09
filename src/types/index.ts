425d5582ae6299d925706a733208c03f92b0355d
Grigory Morozov <morozehc@yandex.ru>
2025-04-09 13:48:20 +0300

    feat: define interface Model for data

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,3 +2,13 @@ export interface IItem {
   id:string;
   name: string
 }
+
+
+// интерфейс для работы с данными
+
+export interface IToDoModel {
+  items: IItem[]
+  addItem(item: IItem): void
+  removeItem(id: string): void
+
+}
