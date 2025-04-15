55f85684054a7b4b12903bb2398262de58ab415c
Grigory Morozov <morozehc@yandex.ru>
2025-04-15 14:34:11 +0300

    feat: create arr cards for todo for view on page with help class Page

--- src/index.ts
+++ src/index.ts
@@ -9,16 +9,19 @@ const contentElement = document.querySelector('.content') as HTMLElement;
 
 const itemTemplate = document.querySelector('#todo-item-template') as HTMLTemplateElement;
 const formTemplate = document.querySelector('#todo-form-template') as HTMLTemplateElement;
-
+// создаем экземпляр страницы для отображения всех элементов страницы
 const page = new Page(contentElement);
-
+// создание модели данных
+const todoArray = new ToDoModel();
+// модель данных- это наш массив данных, мы можем получать его и от сервера
+todoArray.items = todos;
 // создаем экземпляр формы
 const todoForm = new Form(formTemplate)
 // добавили обработчик к этой форме
 todoForm.setHandler(handleSubmitForm)
 // отобразим форму
 page.formContainer = todoForm.render();
-
+// обработчик формы
 function handleSubmitForm(data:string){
   // data - строка из поля ввода
     const todoItem = new Item(itemTemplate);
@@ -28,13 +31,14 @@ function handleSubmitForm(data:string){
 
 }
 
+// выводим из массива данных - создаем массив карточек который  потом выведем на страницу
 
-todos.forEach((item)=>{
+page.todoContainer= todoArray.items.map((item)=>{
+  // одеваем данные карточки в разметку
   const todoItem = new Item(itemTemplate)
   const itemElement = todoItem.render(item)
-  contentElement.prepend(itemElement)
+  return itemElement
 })
 
 
-const todoArray = new ToDoModel();
-todoArray.items = todos;
+
