7cffab209f244c3838b3d0aa0df6a0725a74a9bd
Grigory Morozov <morozehc@yandex.ru>
2025-04-15 18:37:23 +0300

    feat: create interface for class Item and interface for constructor that returns object Item

--- src/components/item.ts
+++ src/components/item.ts
@@ -1,6 +1,21 @@
 import {IItem} from '../types';
 // слой отображения/представления - корректное отображение и взаимодействие пользователя с карточкой
-export class Item {
+
+// интерфейс для элемента который создается как экземпляр этого класса
+// описывает будущий объект
+export interface IViewItem {
+  id:string;
+  name:string;
+  render(item:IItem):HTMLElement
+}
+
+// интерфейс для конструктора описывает параметры кот принимает конструктор 
+// и какой объект дает навыходе когда мы создаем экземпляр
+export interface IViewItemConstructor {
+  new (template: HTMLTemplateElement): IViewItem
+}
+
+export class Item implements IViewItem{
 
   protected itemElement: HTMLElement;
   protected title: HTMLElement;
