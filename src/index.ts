aae2611010d7f1807e1a70e2e5efecf2acc56b51
Grigory Morozov <morozehc@yandex.ru>
2025-04-15 13:37:59 +0300

    feat: add form on our page

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,25 @@ import {todos} from './utils/constants';
 import {Item} from './components/Item';
 import {Form} from './components/Form';
 import {ToDoModel} from './components/ToDoModel';
+import {Page} from './components/Page';
 
-const contentElement = document.querySelector('.todos__list') 
+const contentElement = document.querySelector('.content') as HTMLElement;
 
-const template = document.querySelector('#todo-item-template') as HTMLTemplateElement;
-const formElement = document.querySelector('.todos__form') as HTMLFormElement;
+const itemTemplate = document.querySelector('#todo-item-template') as HTMLTemplateElement;
+const formTemplate = document.querySelector('#todo-form-template') as HTMLTemplateElement;
 
-const todoForm = new Form(formElement,handleSubmitForm);
+const page = new Page(contentElement);
+
+// создаем экземпляр формы
+const todoForm = new Form(formTemplate)
+// добавили обработчик к этой форме
+todoForm.setHandler(handleSubmitForm)
+// отобразим форму
+page.formContainer = todoForm.render();
 
 function handleSubmitForm(data:string){
   // data - строка из поля ввода
-    const todoItem = new Item(template);
+    const todoItem = new Item(itemTemplate);
     const itemElement = todoItem.render({id: '8', name: data});
     contentElement.prepend(itemElement);
     todoForm.clearValue();
@@ -22,7 +30,7 @@ function handleSubmitForm(data:string){
 
 
 todos.forEach((item)=>{
-  const todoItem = new Item(template)
+  const todoItem = new Item(itemTemplate)
   const itemElement = todoItem.render(item)
   contentElement.prepend(itemElement)
 })
