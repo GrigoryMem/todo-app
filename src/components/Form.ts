402db3f46620618be4a21f92140f67c6070261b0
Grigory Morozov <morozehc@yandex.ru>
2025-04-15 13:33:05 +0300

    feat: class Form add fields:buttonText,placeHolder,setHandler

--- src/components/Form.ts
+++ src/components/Form.ts
@@ -1,18 +1,46 @@
 // отвечает за форму за ее отображение и взаимодействие с пользователем
 //за разметку формы
+// интерфейс предусмвтривает основной функционал формы
+export interface IForm {
+  buttonText: string;
+  placeholder: string;
+  setHandler(handler: Function): void
+  render(): HTMLFormElement;
+  setValue(data:string):void;
+  getValue():string;
+  clearValue():void
+}
+
+export interface IFormConstructor {
+  new (formTemplate: HTMLTemplateElement): IForm
+}
+
+
+
+
 export  class Form {
   protected formElement: HTMLFormElement;
   protected inputField: HTMLInputElement;
+  protected handleFormSubmit: Function;
+  protected submitButton: HTMLButtonElement;
 
-  constructor(formElement: HTMLFormElement, protected handleFormSubmit: Function) {
-    this.formElement = formElement;
+  constructor(formTemplate: HTMLTemplateElement) {
+    // нужные элементы формы сохраняем в полях класса
+    this.formElement = formTemplate.content.querySelector('.todo-form').cloneNode(true) as HTMLFormElement;
     this.inputField = this.formElement.querySelector('.todo-form__input') as HTMLInputElement;
+    this.submitButton = this.formElement.querySelector('.todo-form__submit-btn');
+
     this.formElement.addEventListener('submit',(evt)=>{
       evt.preventDefault();
-      // обработчик действий пользователя содержится вне класса
+      // обработчик действий пользователя содержится вне класса , но является полем класса
       this.handleFormSubmit(this.inputField.value)
-      console.log("this.inputField.value")
+
     })
+
+  }
+
+  setHandler(handleFormSubmit: Function){
+    this.handleFormSubmit = handleFormSubmit
   }
   
   render(){
@@ -29,6 +57,14 @@ export  class Form {
 
   clearValue(){
     this.formElement.reset();
+  }
+
+  set buttonText(data: string) {
+    this.submitButton.textContent = data;
+  }
+
+  set placeHolder(data: string) {
+    this.inputField.placeholder = data;
   }
 }
 
